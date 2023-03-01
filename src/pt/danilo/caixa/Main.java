e23ea49a48d000f361972bf204fd98e9f254ea25
alunoESGTS <alunoESGTS@SI2_08.ESG.IPSantarem.local>
2023-03-01 13:56:49 +0000

    Initial Commit

---
+++ src/pt/danilo/caixa/Main.java
@@ -0,0 +1,14 @@
+package pt.danilo.caixa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("******");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("******");
+
+	}
+
+}
