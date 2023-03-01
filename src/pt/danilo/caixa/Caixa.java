2f575570ad7ac36dba8c88fb38951ef12289239f
alunoESGTS <alunoESGTS@SI2_08.ESG.IPSantarem.local>
2023-03-01 16:04:25 +0000

    Add character parameter

--- src/pt/danilo/caixa/Caixa.java
+++ src/pt/danilo/caixa/Caixa.java
@@ -4,10 +4,16 @@ public class Caixa {
 
 	private int width;
 	private int height;
-
+	private char symbol;
+	
 	public Caixa(int width, int height) {
+		this(width, height, '*');
+	}
+
+	public Caixa(int width, int height, char symbol) {
 		this.width = width;
 		this.height = height;
+		this.symbol = symbol;
 	}
 	
 	public void print() {
@@ -24,18 +30,18 @@ public class Caixa {
 
 	private void printTopOrBottom() {
 		for (int i = 0; i < width; i++) {
-			System.out.print("*");
+			System.out.print(symbol);
 		}
 		System.out.println();
 	}
 	
 	private void printLeftAndRight() {
 		for (int i = 0; i <  height - 2; i++) {
-			System.out.print("*");
+			System.out.print(symbol);
 			for (int j = 0; j < width - 2; j++) {
 				System.out.print(" ");
 			}
-			System.out.println("*");
+			System.out.println(symbol);
 		}
 	}
 	
--- src/pt/danilo/caixa/Main.java
+++ src/pt/danilo/caixa/Main.java
@@ -3,9 +3,11 @@ package pt.danilo.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		Caixa caixa = new Caixa(4,4);
+		Caixa caixa = new Caixa(4,4, '#');
 		caixa.print();
-;
+		
+		Caixa caixa2 = new Caixa(5,6);
+		caixa2.print();
 		
 	}
 	
