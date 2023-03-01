d87f31fe79cc029da03151377c722a566f575e5c
alunoESGTS <alunoESGTS@SI2_08.ESG.IPSantarem.local>
2023-03-01 14:33:56 +0000

    Revert "Criar printTopOrBottom"
    This reverts commit 5da9a7525f79f7959522ed793f4f81fa451579a8.

--- src/pt/danilo/caixa/Main.java
+++ src/pt/danilo/caixa/Main.java
@@ -3,14 +3,12 @@ package pt.danilo.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		printTopOrBottom();
+		System.out.println("******");
 		System.out.println("*    *");
 		System.out.println("*    *");
 		System.out.println("*    *");
-		printTopOrBottom();
+		System.out.println("******");
+
 	}
 
-	public static void printTopOrBottom() {
-		System.out.println("******");	
-	}
 }
