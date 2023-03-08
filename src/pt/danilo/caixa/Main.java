f50141e01f02aecb102c862ba4cee940eb5ed499
alunoESGTS <alunoESGTS@SI2_08.ESG.IPSantarem.local>
2023-03-08 15:48:30 +0000

    Handle NotEnoughHeight

--- src/pt/danilo/caixa/Main.java
+++ src/pt/danilo/caixa/Main.java
@@ -2,6 +2,7 @@ package pt.danilo.caixa;
 
 import java.util.Scanner;
 
+import pt.danilo.caixa.errors.NotEnoughHeightException;
 import pt.danilo.caixa.errors.NotEnoughWidthException;
 
 public class Main {
@@ -16,11 +17,14 @@ public class Main {
 		int height = leitor.nextInt();
 		
 		Caixa caixa = new Caixa(width,height, '#');
+		
 		try {
 		caixa.print();
-		} catch(NotEnoughWidthException) {
-			System.out.println("Ocorreu um erro ao desenhar a caixa!");
-			//System.out.println(e.getMessage());
+		} catch(NotEnoughWidthException e) {
+			System.out.println("Ocorreu um erro ao desenhar a caixa! A Largura deve ser superior a 3");
+		}
+		catch(NotEnoughHeightException e) {
+		System.out.println("Ocorreu um erro ao desenhar a caixa! A Altura deve ser superior a 3");
 		}
 		
 	}
